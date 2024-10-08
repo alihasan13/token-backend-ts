eaee49a5d60fbfdae98377e3173e143833f360d1
Ali Hasan <alihasan13@users.noreply.github.com>
2024-10-08 23:53:31 +0600

    git access token func added

--- api/github-adapter.ts
+++ api/github-adapter.ts
@@ -21,3 +21,12 @@ export async function getGithubUser(code:string) {
     const token = await getAccessToken(code)
     return getuser(token)
 }
+
+async function getAccessToken(code: string){
+    const {data} = await axios.post<AccessTokenResponse>(TOKEN_URL, {
+        client_id: process.env.GITHUB_CLIENT_ID,
+        client_secret: process.env.GITHUB_CLIENT_SECRET,
+        code
+    })
+    return data.access_token
+}
